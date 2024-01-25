4e656e63d038c0439074b9924e89c8125ad885a8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-01-25 22:13:21 +0400

    feat [ServerRequestWizard] Uri from server variables.

---
+++ src/ServerRequestWizard.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\Psr7Wizard;
+
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+use function preg_match;
+
+class ServerRequestWizard
+{
+    public function __construct(
+        private readonly ServerRequestFactoryInterface $serverRequestFactory,
+        private readonly StreamFactoryInterface $streamFactory,
+        private readonly UploadedFileFactoryInterface $uploadedFileFactory,
+        private readonly UriFactoryInterface $uriFactory,
+    ) {}
+
+    public function fromGlobals(
+        array $serverParams = null,
+        array $queryParams = null,
+        array $cookieParams = null,
+        array $uploadedFiles = null,
+        array $parsedBody = null
+    ): ServerRequestInterface {
+        $serverParams ??= $_SERVER;
+        $queryParams ??= $_GET;
+        $cookieParams ??= $_COOKIE;
+        $uploadedFiles ??= $_FILES;
+        $parsedBody ??= $_POST;
+
+        return $this->serverRequestFactory
+            ->createServerRequest(
+                method: $serverParams['REQUEST_METHOD'] ?? 'GET',
+                uri: $this->createUriFromServer($serverParams),
+                serverParams: $serverParams
+            )
+        ;
+    }
+
+    private function createUriFromServer(array $serverParams): UriInterface
+    {
+        $scheme = $serverParams['REQUEST_SCHEME'] ?? '';
+
+        if ('' === $scheme) {
+            $scheme = 'off' !== ($serverParams['HTTPS'] ?? null) ? 'https' : 'http';
+        }
+
+        $host = $serverParams['HTTP_HOST'] ?? $serverParams['SERVER_NAME'] ?? 'localhost';
+
+        if ('' !== ($port = $serverParams['SERVER_PORT'])
+            && 1 !== preg_match('/:(\d+)$/', $host)) {
+            $host .= ':'.$port;
+        }
+
+        if ('' !== ($requestUriWithQuery = $serverParams['REQUEST_URI'] ?? '')) {
+            $requestUri = $requestUriWithQuery;
+        } elseif ('' !== ($phpSelf = $serverParams['PHP_SELF'] ?? '')) {
+            $requestUri = $phpSelf.($query = $serverParams['QUERY_STRING'] ?? '') ? '?'.$query : '';
+        }
+
+        $uriString = $scheme.'://'.$host.($requestUri ?? '');
+
+        return $this->uriFactory->createUri($uriString);
+    }
+}
